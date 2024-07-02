a57e9135ed0537bd07a4842ab6a262f5b279c9ea
nel850 <nelsonmunyua8@gmail.com>
2024-07-02 14:35:34 +0300

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body classname={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
